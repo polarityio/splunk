9bae132918e9d08170590b2d73289989be06e82a
Ed <ed@polarity.io>
2019-09-27 22:20:04 -0400

    Initial overhaul of Splunk integration v3.0.0-beta

--- config/config.js
+++ config/config.js
@@ -1,101 +1,150 @@
 module.exports = {
-    "name": "Splunk",
-    "acronym":"Splunk",
-    "logging": { level: 'debug'},
-    "entityTypes": ['IPv4', 'IPv6', 'hash', 'email'],
-    "styles": [
-     "./styles/splunk.less"
-     ],
-     "block": {
-     "component": {
-     "file": "./components/block.js"
-     },
-     "template": {
-     "file": "./templates/block.hbs"
-     }
-     },
-    "options":[
-        {
-            "key"          : "host",
-            "name"         : "Splunk Host",
-            "description"  : "url for splunk instance, please do not include https:// in your url",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "port",
-            "name"         : "port for splunk rest service - deafult for splunk is 8089",
-            "description"  : "",
-            "default"      : "8089",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "username",
-            "name"         : "Username",
-            "description"  : "Splunk Account Username",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "password",
-            "name"         : "Password",
-            "description"  : "Splunk Account Password",
-            "default"      : "",
-            "type"         : "password",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "searchStringIP",
-            "name"         : "Search String for IPs",
-            "description"  : "Search you want to perform in splunk for example: search index=logs ip = {{ENTITY}} | head 10",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "scheme",
-            "name"         : "Scheme",
-            "description"  : "scheme for splunk rest service - default for splunk is https",
-            "default"      : "https",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "version",
-            "name"         : "Version",
-            "description"  : "Version of Splunk that you are operating",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "uiHostname",
-            "name"         : "UI Hostname",
-            "description"  : "UI Hostname of the Splunk instance you are running",
-            "default"      : "",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        },
-        {
-            "key"          : "autoCancel",
-            "name"         : "Auto Cancel",
-            "description"  : "Number of seconds for a query to run before canceling",
-            "default"      : "20",
-            "type"         : "text",
-            "userCanEdit" : true,
-            "adminOnly"    : false
-        }
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'Splunk Enterprise',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'SE',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description:
+    'Splunk Enteprise is the fastest way to aggregate, analyze and get answers from your machine data with the help machine learning and real-time visibility.',
+  entityTypes: ['IPv4', 'IPv6', 'hash', 'email'],
+  /**
+   * An array of style files (css or less) that will be included for your integration. Any styles specified in
+   * the below files can be used in your custom template.
+   *
+   * @type Array
+   * @optional
+   */
+  styles: ['./styles/se.less'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  block: {
+    component: {
+      file: './components/se-block.js'
+    },
+    template: {
+      file: './templates/se-block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/se-summary.js'
+    },
+    template: {
+      file: './templates/se-summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
 
-    ]
+    rejectUnauthorized: true
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'url',
+      name: 'Base Splunk Enterprise URL',
+      description:
+        'The base URL for the Splunk Enterprise REST API including the schema (i.e., https://) and port (e.g., https://mysplunk:8089)',
+      type: 'text',
+      default: '',
+      userCanEdit: false,
+      adminOnly: true
+    },
+    {
+      key: 'searchAppUrl',
+      name: 'Splunk Search App URL',
+      description:
+        'The URL for the Splunk Enterprise Search App including schema (i.e., https://) and port (e.g., https://mysplunk:9000/en-US/app/search/search). This option must be set to "User can view and edit" or "User can view only".',
+      type: 'text',
+      default: '',
+      userCanEdit: false,
+      adminOnly: true
+    },
+    {
+      key: 'apiToken',
+      name: 'Valid Splunk Token',
+      description: 'Valid Splunk Authentication Token',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'searchString',
+      name: 'Splunk Search String',
+      description:
+        'Splunk Search String to execute. The string `{{ENTITY}}` will be replace by the looked up indicator. For example: search index=logs value={{ENTITY}} | head 10',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+
+    {
+      key: 'summaryFields',
+      name: 'Summary Fields',
+      description:
+        'Comma delimited list of field values to include as part of the summary (no spaces between commas).  These fields must be returned by your search query. This option must be set to "User can view and edit" or "User can view only".',
+      default: '_si,_serial',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'includeFieldNameInSummary',
+      name: 'Include Field Name in Summary',
+      description: 'If checked, field names will be included as part of the summary fields. This option must be set to "User can view and edit" or "User can view only".',
+      default: true,
+      type: 'boolean',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
 };
--- integrations.js
+++ integrations.js
@@ -1,241 +1,231 @@
 'use strict';
 
-let request = require('request');
-let util = require('util');
-let _ = require('lodash');
-let async = require('async');
-let splunkjs = require('splunk-sdk');
-let log = null;
+const request = require('request');
+const config = require('./config/config');
+const async = require('async');
+const fs = require('fs');
 
-//variables to assign an icon to a tag
+let Logger;
+let requestWithDefaults;
 
-/*
- Main function of the integration that takes passes in an entity to another function to call the API.
- Then it takes the returned data and passes that to the notification window for the user
+const MAX_PARALLEL_LOOKUPS = 10;
+
+/**
+ *
+ * @param entities
+ * @param options
+ * @param cb
  */
-
-let num = "Number of Results: ";
-function doLookup(entities, options, cb) {
-    let lookupResults = [];
-    // asynchrous call that passes each entity that is an IPv4 or IPv6 to the lookupEntity Function to call the Splunk rest API
-
-    async.each(entities, function (entityObj, next) {
-        if (entityObj.isIPv4 || entityObj.isIPv6) {
-            _lookupEntityIP(entityObj, options, function (err, resultObject) {
-                // variable that is assigned the url query for Splunk so a user can link directly to that query in Splunk
-                let splunkUrl = options.uiHostname + '/en-US/app/search/search?q=' + options.searchStringIP.replace("{{ENTITY}}", entityObj.value);
-                if (err) {
-                    next(err);
-                } else {
-                    if (!resultObject || typeof(resultObject) != "object") {
-                        log.trace({entity: entityObj.value}, 'No IP Data Found. Caching Miss');
-
-                        lookupResults.push({entity: entityObj, done: null});
-                    } else {
-                        log.debug({entity: entityObj.value, result: resultObject}, 'Found IP Result');
-
-                        lookupResults.push({
-                            entity: entityObj,
-                            data: {
-                                summary: [num + _.size(resultObject)],
-                                details: {
-                                    url: splunkUrl,
-                                    number: _.size(resultObject)}
-
-                            }
-                        });
-                    }
-                    next(null);
-                }
-            });
-        }  else {
-            lookupResults.push({entity: entityObj, data: null}); //Cache the miss for non ipv4, hash and ipv6 values
-            next(null);
-        }
-
-    }, function (err) {
-        cb(err, lookupResults);
-    });
-}
-
-//function that checks the parameters of the options a user provides to connect to an integration and lets them know if the options are valid
-function validateOptions(userOptions, cb) {
-    let errors = [];
-
-    if (typeof userOptions.host.value !== 'string' ||
-        (typeof userOptions.host.value === 'string' && userOptions.host.value.length === 0)) {
-        errors.push({
-            key: 'host',
-            message: 'You must provide a valid Splunk host'
-        })
-    }
-
-    if (typeof userOptions.password.value !== 'string' ||
-        (typeof userOptions.password.value === 'string' && userOptions.password.value.length === 0)) {
-        errors.push({
-            key: 'password',
-            message: 'You must provide a valid password'
-        })
-    }
-
-    if (typeof userOptions.username.value !== 'string' ||
-        (typeof userOptions.username.value === 'string' && userOptions.username.value.length === 0)) {
-        errors.push({
-            key: 'username',
-            message: 'You must provide a valid username'
-        })
-    }
-
-    if (typeof userOptions.port.value !== 'string' ||
-        (typeof userOptions.port.value === 'string' && userOptions.port.value.length === 0)) {
-        errors.push({
-            key: 'port',
-            message: 'You must provide the Port used by your installation of Splunk'
-        })
-    }
-
-    cb(null, errors);
-}
-
-
-// function that takes the error object and makes a payload out of it to be passed to the front end
-var _createJsonErrorPayload = function (msg, pointer, httpCode, code, title, meta) {
-    return {
-        errors: [
-            _createJsonErrorObject(msg, pointer, httpCode, code, title, meta)
-        ]
-    }
-};
-
-
-// function that creates a json error object to allow an error to be passed to the notification window
-var _createJsonErrorObject = function (msg, pointer, httpCode, code, title, meta) {
-    let error = {
-        detail: msg,
-        status: httpCode.toString(),
-        title: title,
-        code: 'Splunk_' + code.toString()
-    };
-
-    if (pointer) {
-        error.source = {
-            pointer: pointer
-        };
-    }
-
-    if (meta) {
-        error.meta = meta;
-    }
-
-    return error;
-};
-
-
-/*
- Function that takes in the entity.value that is being passed server. Then passes the option values in as a wuery to the Splunk server and returns those results.
- Once the results are returned it parses them in more of a usable format. Then passes that data back to the DoLookup function.
- */
-
-var _lookupEntityIP = function (entity, options, done) {
-
-    // variable to establish parameters for the connection
-    let service = new splunkjs.Service({
-        username: options.username,
-        password: options.password,
-        scheme: options.scheme,
-        host: options.host,
-        port: options.port,
-        version: options.version
-    });
-
-    //variable that takes serach string to be executed in Splunk replaces the entity with the entity value
-    let searchQuery = options.searchStringIP.replace("{{ENTITY}}", entity.value);
-
-    log.debug({query: searchQuery}, "Running Search Query");
-
-    //sets the serach parameters for splunk
-    let searchParams = {
-        exec_mode: "normal",
-        search_mode: "normal",
-        auto_cancel: options.autoCancel
-    };
-
-    //executes the search by passing in the parameters and the query
-    service.search(
-        searchQuery,
-        searchParams,
-        function (err, job) {
-            //Checks to see if there is an error in connecting to the server and passes it to the notification window.
-            if (err) {
-                done(_createJsonErrorPayload("Unable to connect to Splunk server", null, '500', '2A', 'Login Failed', {
-                    err: err
-                }));
-                return;
-            }
-
-            // polls to ensure the query is executing
-            job.track({period: 1000}, {
-                done: function (job) {
-                    job.results({}, function (err, results, job) {
-                        //if there is an error retrieving results notifies the user in the notification window
-                        if (err) {
-                            done(_createJsonErrorPayload("Error returning the results from Splunk Server", null,
-                                '500', '2B', 'Results failed to return properly', {
-                                    err: err
-                                }));
-                            return;
-                        }
-
-
-                        //variables that are associate with the field and rows in splunk
-                        let fields = results.fields;
-                        let rows = results.rows;
-
-                        // checks to see if there are hits and if not returns
-                        if (rows.length < 1) {
-                            done(null, null);
-                            return;
-                        }
-
-                        //variable to take the field and row and map it together in a list
-                        let resultObjects = _.map(rows, function (row) {
-                            return _.zipObject(fields, row);
-                        });
-
-
-                        // grabs the _raw fields from the list
-                        let raws = _.map(resultObjects, '_raw');
-
-                        done(null, raws);
-                    });
-                },
-                failed: function (job) {
-                    log.error({job: job}, 'Splunk IP Query Job Failed');
-                    done(_createJsonErrorPayload("Splunk IP Job Failed", null,
-                        '500', '2D', 'Splunk Job Failure', {
-                            job: job
-                        }), null);
-                },
-                error: function (err) {
-                    log.error({err: err, job: job}, 'Splunk IP Query Job had an Error');
-                    done(_createJsonErrorPayload("Splunk IP Job Error", null,
-                        '500', '2E', 'Splunk Job Error', {
-                            err: err,
-                            job: job
-                        }), null);
-                }
-            });
-        }
-    );
-};
-
-
 function startup(logger) {
-    log = logger;
+  let defaults = {};
+  Logger = logger;
+
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+    defaults.cert = fs.readFileSync(config.request.cert);
+  }
+
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+    defaults.key = fs.readFileSync(config.request.key);
+  }
+
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+    defaults.passphrase = config.request.passphrase;
+  }
+
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+    defaults.ca = fs.readFileSync(config.request.ca);
+  }
+
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+    defaults.proxy = config.request.proxy;
+  }
+
+  if (typeof config.request.rejectUnauthorized === 'boolean') {
+    defaults.rejectUnauthorized = config.request.rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(defaults);
+}
+
+function doLookup(entities, options, cb) {
+  let lookupResults = [];
+  let tasks = [];
+  const summaryFields = options.summaryFields.split(',');
+  Logger.debug({ entities }, 'Entities');
+
+  entities.forEach((entity) => {
+    //do the lookup
+    let requestOptions = {
+      method: 'GET',
+      uri: `${options.url}/services/search/jobs/export`,
+      headers: {
+        Authorization: 'Bearer ' + options.apiToken
+      },
+      qs: {
+        output_mode: 'json',
+        search: options.searchString.replace(/{{ENTITY}}/gi, entity.value)
+      },
+      json: false
+    };
+
+    Logger.trace({ requestOptions }, 'Request URI');
+
+    tasks.push(function(done) {
+      requestWithDefaults(requestOptions, function(error, res, body) {
+        if (error) {
+          return done({
+            error,
+            detail: 'Error Executing HTTP Request to Splunk REST API'
+          });
+        }
+
+        let result = {};
+
+        if (res.statusCode === 200) {
+          result = {
+            entity: entity,
+            body: body,
+            search: requestOptions.qs.search
+          };
+        } else if (res.statusCode === 404) {
+          // no result found
+          result = {
+            entity: entity,
+            body: null
+          };
+        } else {
+          // unexpected status code
+          return done({
+            err: body,
+            detail: _formatErrorMessages(body)
+          });
+        }
+
+        done(null, result);
+      });
+    });
+  });
+
+  async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      Logger.error({ err: err }, 'Error');
+      cb(err);
+      return;
+    }
+
+    results.forEach((result) => {
+      Logger.trace({ body: result.body });
+      if (!result.body) {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      } else {
+        try {
+          // Splunk returns newline delimited JSON objects.  As a result we need to
+          // custom parse the data.  We replace newlines with commas and then wrap the
+          // text in an array so the end result is an array of result objects.
+          result.body = JSON.parse(`[${result.body.trim().replace(/\n/g, ',')}]`);
+          // A search result with no results is of the form:
+          //     body: [
+          //       {
+          //         "preview": false,
+          //         "lastrow": true
+          //       }
+          //     ]
+          // Search results with data will always have a `result` property.  We check for the result
+          // to determine if the search had a hit or not.
+          //    body: [
+          //       {
+          //         "preview": false,
+          //         "lastrow": true,
+          //         "result": {}
+          //       }
+          //     ]
+          if (result.body.length > 0 && !result.body[0].result) {
+            lookupResults.push({
+              entity: result.entity,
+              data: null
+            });
+          } else {
+            lookupResults.push({
+              entity: result.entity,
+              data: {
+                summary: [],
+                details: {
+                  results: result.body,
+                  search: result.search,
+                  tags: _getSummaryTags(result.body, summaryFields)
+                }
+              }
+            });
+          }
+        } catch (parseError) {
+          Logger.error({ body: result.body }, 'Error parsing query result body into JSON');
+        }
+      }
+    });
+
+    Logger.debug({ lookupResults }, 'Results');
+    cb(null, lookupResults);
+  });
+}
+
+function _getSummaryTags(results, summaryFields) {
+  const tags = new Map();
+
+  results.forEach((item) => {
+    summaryFields.forEach((field) => {
+      const summaryField = item.result[field];
+      if (summaryField) {
+        tags.set(`${field}${summaryField}`, {
+          field: field,
+          value: summaryField
+        });
+      }
+    });
+  });
+
+  return Array.from(tags.values());
+}
+
+function _formatErrorMessages(err) {
+  let formattedMessage = '';
+  if (Array.isArray(err.messages)) {
+    err.messages.forEach((message) => {
+      formattedMessage += message.text;
+    });
+  } else if (typeof err === 'string') {
+    formattedMessage = err;
+  }
+
+  return formattedMessage;
+}
+
+function validateOptions(userOptions, cb) {
+  let errors = [];
+  if (
+    typeof userOptions.url.value !== 'string' ||
+    (typeof userOptions.url.value === 'string' && userOptions.url.value.length === 0)
+  ) {
+    errors.push({
+      key: 'url',
+      message: 'You must provide a valid Splunk URL'
+    });
+  }
+
+  if (typeof userOptions.url.value === 'string' && userOptions.url.value.endsWith('/')) {
+    errors.push({
+      key: 'url',
+      message: 'The Splunk URL should not end with a forward slash ("/")'
+    });
+  }
+
+  cb(null, errors);
 }
 
 module.exports = {
-    doLookup: doLookup,
-    startup: startup,
-    validateOptions: validateOptions
+  doLookup: doLookup,
+  startup: startup,
+  validateOptions: validateOptions
 };
