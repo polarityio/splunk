0462ec7b04740df23d4cb4a24e5e12d51bef9e8d
Ed <ed@polarity.io>
2024-04-23 21:44:11 -0400

    Encode search query

--- integration.js
+++ integration.js
@@ -121,8 +121,8 @@ const doLookup = (entities, options, cb) => {
                 summary: [],
                 details: {
                   results: searchResponseBody,
-                  search: result.searchQuery,
-                  searchAppQuery: result.searchAppQuery,
+                  search: encodeURIComponent(result.searchQuery),
+                  searchAppQuery: encodeURIComponent(result.searchAppQuery),
                   searchType: result.searchType,
                   tags: _getSummaryTags(result.searchResponseBody, summaryFields, options)
                 }
